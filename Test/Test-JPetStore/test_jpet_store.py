ca76032b00afb07edcd462470cbc45ae3924236f
jdescolegl <jeronimo.descole@globallogic.com>
2022-05-30 13:46:03 -0300

    starting with allure

--- Test/Test-JPetStore/test_jpet_store.py
+++ Test/Test-JPetStore/test_jpet_store.py
@@ -1,4 +1,5 @@
 import pytest
+import allure
 from selenium.webdriver.support.wait import WebDriverWait
 from Src.PageObject.j_pet_store_page import HomePage
 
@@ -7,7 +8,7 @@ class TestJpetStore:
     @pytest.fixture(scope="class")
     def home_page(self):
         _url = "https://petstore.octoperf.com/"
-        self.home_page = HomePage(_url, headless=False)
+        self.home_page = HomePage(_url, headless=True)
         yield self.home_page
         self.home_page.driver.quit()
 
@@ -15,9 +16,11 @@ class TestJpetStore:
         heading_title = home_page.get_heading()
         assert heading_title == "Welcome to JPetStore 6"
 
+    @pytest.mark.asd
     def test_enter_the_store_link(self, home_page):
         enter_store = home_page.get_enter_store()
         enter_store.click()
+        allure.attach(home_page.driver.save_screenshot('enter_store'), name="enter_store", attachment_type="PNG")
         wait = WebDriverWait(home_page.driver, 10)
         wait.until(lambda driver: home_page.driver.current_url != "https://petstore.octoperf.com/")
         assert home_page.driver.current_url == "https://petstore.octoperf.com/actions/Catalog.action"
